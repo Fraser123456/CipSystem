615e9131b5813aae81b1dd54c0223c2c5592f2ca
Fraser_Carpenter <fraserac3@gmail.com>
2020-10-31 14:32:25 +0200

    got the navbar with the master page working

---
+++ CipSystem-SPA/src/app/navbar/navbar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild, EventEmitter, Input, Output } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { AuthService } from "../Services/auth.service";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) { }
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+}
